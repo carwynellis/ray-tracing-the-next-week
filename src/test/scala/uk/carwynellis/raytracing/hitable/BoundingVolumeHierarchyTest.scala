364b9e2f110138ae786fb745ed00b572d3839656
Carwyn Ellis <carwynellis@gmail.com>
2018-10-21 18:31:02 +0100

    Added testcases to exercise BVH hit method

--- src/test/scala/uk/carwynellis/raytracing/hitable/BoundingVolumeHierarchyTest.scala
+++ src/test/scala/uk/carwynellis/raytracing/hitable/BoundingVolumeHierarchyTest.scala
@@ -1,14 +1,15 @@
 package uk.carwynellis.raytracing.hitable
 
 import org.scalatest.{FunSuite, Matchers}
-import uk.carwynellis.raytracing.Vec3
+import uk.carwynellis.raytracing.{HitRecord, Ray, Vec3}
 import uk.carwynellis.raytracing.material.Lambertian
 import uk.carwynellis.raytracing.texture.ConstantTexture
 
 class BoundingVolumeHierarchyTest extends FunSuite with Matchers {
 
-  private val sphere = Sphere(Vec3(0,0,0), 1.0, Lambertian(ConstantTexture(Vec3(0, 0, 0))))
-  private val anotherSphere = Sphere(Vec3(1,2,3), 5.0, Lambertian(ConstantTexture(Vec3(4, 4, 4))))
+  private val material = Lambertian(ConstantTexture(Vec3(1, 1, 1)))
+  private val sphere = Sphere(Vec3(0,0,0), 1.0, material)
+  private val anotherSphere = Sphere(Vec3(1,2,3), 5.0, material)
 
   test("should build a node with both child nodes set to the single element for a list of a single hitable") {
     val hitables = List(sphere)
@@ -34,6 +35,33 @@ class BoundingVolumeHierarchyTest extends FunSuite with Matchers {
     val result = BoundingVolumeHierarchy.ofHitables(hitables, 0, 0)
 
     // TODO - inspect the result - this is a cludge to check that the recursion terminates correctly
+  }
+
+  test("hit should return a hit record for a ray that hits one of the spheres") {
+    val hitables = List(sphere, sphere)
+
+    val bvh = BoundingVolumeHierarchy.ofHitables(hitables, 0, 0)
+
+    val r = Ray(Vec3(0,0,0), Vec3(3,3,3))
+
+    bvh.hit(r, 0, 1) shouldBe Some(HitRecord(
+      0.19245008972987526,
+      0.0,
+      0.0,
+      Vec3(0.5773502691896257,0.5773502691896257,0.5773502691896257),
+      Vec3(0.5773502691896257,0.5773502691896257,0.5773502691896257),
+      material
+    ))
+  }
+
+  test("hit should not return a hit record for a ray that misses one of the spheres") {
+    val hitables = List(sphere, sphere)
+
+    val bvh = BoundingVolumeHierarchy.ofHitables(hitables, 0, 0)
+
+    val r = Ray(Vec3(8,8,8), Vec3(3,3,3))
+
+    bvh.hit(r, 0, 1) shouldBe None
   }
 
 }
